e6d382dc87cd36a638f019d31b86794521f783af
Dario Carella <dario.kar@gmail.com>
2016-12-24 20:40:22 +0100

    Add orbiter logic and longpress actions (clear)

--- app/components/Project/index.jsx
+++ app/components/Project/index.jsx
@@ -1,31 +1,36 @@
 import React, { PureComponent, PropTypes } from 'react';
-import datePropType from '../../propTypes/date';
-import style from './style.css';
 import contrast from 'contrast';
 import numeral from 'numeral';
+import classnames from 'classnames';
+import datePropType from '../../propTypes/date';
+import style from './style.css';
 
 
 class Project extends PureComponent {
   constructor(props) {
     super(props);
 
-    // internal props
-    this.clockCycleIteration = 0;
-    this.timer = false;
-
-    // initial state
-    this.state = {
-      stackedTime: this.getTime(),
-      initials: '',
-    };
-
     // binding methods
-    this.getTime = this.getTime.bind(this);
     this.clock = this.clock.bind(this);
     this.startTimer = this.startTimer.bind(this);
     this.stopTimer = this.stopTimer.bind(this);
     this.timerToggleHandler = this.timerToggleHandler.bind(this);
+    this.pointerDownHandler = this.pointerDownHandler.bind(this);
+    this.pointerUpHandler = this.pointerUpHandler.bind(this);
+    this.longPressHandler = this.longPressHandler.bind(this);
   }
+
+  /** Internal props **/
+  state = {
+    stackedTime: this.getTime(this.props.streaks, this.props.currentTimer),
+    initials: '',
+    isOptionsPanelOpen: false,
+  }
+  clockCycleIteration = Math.round((this.state.stackedTime / 1000) % 60);
+  orbiterRotation = 0;
+  timer = false;
+  longPressTimeout = null;
+  isTimerToggleDisabled = false;
 
   /** React lifecycle **/
   componentWillMount() {
@@ -33,9 +38,16 @@ class Project extends PureComponent {
       this.startTimer();
     }
 
+    this.updateOrbiterRotation();
+
     this.setState({
       initials: this.getInitials(),
     });
+  }
+  componentDidMount() {
+    this.ref.addEventListener('click', this.timerToggleHandler);
+    this.ref.addEventListener('mousedown', this.pointerDownHandler);
+    this.ref.addEventListener('touchstart', this.pointerDownHandler);
   }
   componentWillReceiveProps(nextProps) {
     if (!this.props.currentTimer && nextProps.currentTimer) {
@@ -45,19 +57,25 @@ class Project extends PureComponent {
       // stop timer
       this.stopTimer();
     }
+
+    if (!nextProps.streaks.length && !nextProps.currentTimer) {
+      this.orbiterRotation = this.clockCycleIteration = 0;
+      this.setState({ stackedTime: 0 });
+    }
   }
   componentWillUnmount() {
     this.stopTimer();
+    this.ref.removeEventListener('click', this.timerToggleHandler);
+    this.ref.removeEventListener('mousedown', this.pointerDownHandler);
+    this.ref.removeEventListener('touchstart', this.pointerDownHandler);
   }
 
   /** Internal methods **/
   getInitials() {
     const words = this.props.label.split(/[\s,]+/);
-    return words.slice(0, 3).map(w => w[0]).reduce((f, s) => f + s);
+    return words.slice(0, 3).map(w => w[0].toUpperCase()).reduce((f, s) => f + s);
   }
-  getTime() {
-    const { streaks, currentTimer } = this.props;
-
+  getTime(streaks, currentTimer) {
     let stackedTime = streaks
       .map(s => s[1].getTime() - s[0].getTime())  // get time diffs
       .reduce((pv, cv) => pv + cv, 0);            // sum diffs
@@ -71,11 +89,13 @@ class Project extends PureComponent {
   }
   clock() {
     // progressively sum 1s and sync every 60s
-    if (this.clockCycleIteration++ < 60) {
+    if (this.clockCycleIteration++ < 59) {
+      this.updateOrbiterRotation();
       this.setState({ stackedTime: this.state.stackedTime + 1000 });
     } else {
-      this.setState({ stackedTime: this.getTime() });
       this.clockCycleIteration = 0;
+      this.updateOrbiterRotation();
+      this.setState({ stackedTime: this.getTime(this.props.streaks, this.props.currentTimer) });
     }
   }
   startTimer() {
@@ -89,17 +109,108 @@ class Project extends PureComponent {
     }
   }
   timerToggleHandler() {
+    if (this.isTimerToggleDisabled) {
+      return;
+    }
+
     if (this.props.currentTimer) {
       this.props.stop(this.props.id);
     } else {
       this.props.start(this.props.id);
     }
   }
+  updateOrbiterRotation() {
+    this.orbiterRotation = this.orbiterRotation || 0; // first time starts from 0
+    // new rotation
+    const nR = this.clockCycleIteration * 6;          // 360° / 60s = 6°/s
+    // current apparent rotation
+    let aR = this.orbiterRotation % 360;
+
+    if (aR < 0) {
+      aR += 360;
+    }
+    if (aR < 180 && nR > (aR + 180)) {
+      this.orbiterRotation -= 360;
+    }
+    if (aR >= 180 && nR <= (aR - 180)) {
+      this.orbiterRotation += 360;
+    }
+    this.orbiterRotation += (nR - aR);
+
+    return this.orbiterRotation;
+  }
+  pointerDownHandler() {
+    this.longPressTimeout = setTimeout(this.longPressHandler, 750);
+
+    window.addEventListener('mouseup', this.pointerUpHandler);
+    window.addEventListener('touchend', this.pointerUpHandler);
+  }
+  pointerUpHandler(e) {
+    this.longPressTimeout = clearTimeout(this.longPressTimeout);
+    window.removeEventListener('mouseup', this.pointerUpHandler);
+    window.removeEventListener('touchend', this.pointerUpHandler);
+
+    if (this.state.isOptionsPanelOpen) {
+      // get coordinates relative to timer center
+      const bb = this.ref.getBoundingClientRect();
+      const { top, left, height, width } = bb;
+      let x = -(left + (width / 2));
+      let y = -(top + (height / 2));
+      if (e.type === 'mouseup') {
+        x += e.pageX;
+        y += e.pageY;
+      } else {
+        if (e.touches.length > 0) {
+          x += e.touches[0].x;
+          y += e.touches[0].y;
+        } else {
+          x += e.changedTouches[0].x;
+          y += e.changedTouches[0].y;
+        }
+      }
+
+      // execute corner action
+      if (x < -width / 3 && y < -height / 3) {
+        console.log(`[${this.state.initials}] TOP LEFT CORNER ACTION (not defined yet)`);
+      } else if (x > width / 3 && y < -height / 3) {
+        console.log(`[${this.state.initials}] TOP RIGHT CORNER ACTION (not defined yet)`);
+      } else if (x > width / 3 && y > height / 3) {
+        console.log(`[${this.state.initials}] BOTTOM RIGHT CORNER ACTION (clear)`);
+        this.props.clear(this.props.id);
+      } else if (x < -width / 3 && y > height / 3) {
+        console.log(`[${this.state.initials}] BOTTOM LEFT CORNER ACTION (not defined yet)`);
+      } else {
+        console.log(`[${this.state.initials}] NO ACTION`);
+      }
+
+      this.setState({
+        isOptionsPanelOpen: false,
+      });
+
+      setTimeout(() => {
+        this.isTimerToggleDisabled = false;
+      }, 400);
+    }
+  }
+  longPressHandler() {
+    this.isTimerToggleDisabled = true;
+    this.setState({
+      isOptionsPanelOpen: true,
+    });
+  }
 
   /** Render **/
   render() {
-    const { label, picture, color, hotkey } = this.props;
-    const { stackedTime, initials } = this.state;
+    const { label, picture, color, hotkey, currentTimer } = this.props;
+    const { stackedTime, initials, isOptionsPanelOpen } = this.state;
+
+    const classes = {
+      base: classnames(style.base, {
+        [style.running]: !!currentTimer,
+        [style.panelOpen]: isOptionsPanelOpen,
+      }),
+      clearAction: classnames(style.action, style.clear),
+    };
 
     const inlineStyles = {
       base: {
@@ -108,25 +219,29 @@ class Project extends PureComponent {
       infoWrapper: {
         color: color && contrast(color) === 'light' ? '#000' : '#FFF',
       },
-      background: {
-        backgroundImage: `url(${picture})`,
-      },
       orbiter: {
         backgroundColor: color,
+        transform: `rotate(${this.orbiterRotation}deg)`,
+      },
+      background: {
+        backgroundImage: `url(${picture})`,
       },
     };
 
     return (
       <div
-        className={style.base}
+        className={classes.base}
         style={inlineStyles.base}
         title={label}
-        onClick={this.timerToggleHandler}
+        ref={r => { this.ref = r; }}
       >
         <div className={style.infoWrapper} style={inlineStyles.infoWrapper}>
           <div className={style.label}>{initials}</div>
           <div className={style.stackedTime}>{numeral(stackedTime / 1000).format('00:00:00')}</div>
           <div className={style.hotkey}>{hotkey}</div>
+        </div>
+        <div className={style.actionsWrapper}>
+          <div className={classes.clearAction}></div>
         </div>
         <div className={style.orbiter} style={inlineStyles.orbiter}></div>
         <div className={style.background} style={inlineStyles.background} />
